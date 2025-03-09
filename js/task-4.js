87c6c145b05dfb67c2570f5e94e23411bef68ee5
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-03-09 23:29:59 +0100

    add

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+const loginUser = document.querySelector("#login-form")
+
+
+loginUser.addEventListener("submit", function (event){
+    event.preventDefault();
+    
+const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+      
+    
+  if (login === "" || password === "") {
+      alert ('All form fields must be filled in')
+      return;
+  }
+    
+    
+  const formData = {
+    email: email,
+    password: password,
+  };
+    
+    
+    console.log(`Login: ${login}, Password: ${password}`);
+    
+  form.reset();
+})
