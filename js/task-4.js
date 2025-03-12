36d34af72a58ed25b8f39d4bd40130aa8ab9d5fd
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-03-12 22:38:38 +0100

    uodate

--- js/task-4.js
+++ js/task-4.js
@@ -1,26 +1,21 @@
-const loginUser = document.querySelector(".login-form")
+const accumulatedData = {};
 
+const loginForm = document.querySelector(".login-form");
 
-loginUser.addEventListener("submit", function (event){
-    event.preventDefault();
-    
-const email = form.elements.email.value.trim();
-  const password = form.elements.password.value.trim();
-      
-    
-  if (login === "" || password === "") {
-      alert ('All form fields must be filled in')
-      return;
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+    return;
   }
-    
-    
-  const formData = {
-    email: email,
-    password: password,
-  };
-    
-    
-    console.log(`Login: ${email}, Password: ${password}`);
-    
-   loginUser.reset();
-})
+
+  accumulatedData[email] = password;
+
+  console.log("Accumulated data:", accumulatedData);
+
+  loginForm.reset(); 
+});
--- js/task-5.js
+++ js/task-5.js
@@ -1,24 +1,15 @@
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
+const body = document.body;
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
 
-const changeElem = document.body;
-const buttonColorChange = document.querySelector(".change-color")
-const colorChange = docment.querySelector(".color")
-
-buttonColorChange.addEventListener("click", () => {
-
-    const randomColor = getRandomHexColor();
-
-  changeElem.style.backgroundColor = randomColor;
-  colorChange.textContent = randomColor;
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
 });
-
-
-
-
-
