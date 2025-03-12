afb231befbb4c7e6ae3e3c765b3c21f27afc97b0
Ann-teres <anna.t.e.r.e.777@gmail.com>
2025-03-12 05:43:51 +0100

    add

--- js/task-5.js
+++ js/task-5.js
@@ -14,26 +14,11 @@ btnChangeColorElem.addEventListener("click", () => {
 
     const random = getRandomHexColor();
 
-  changeElem.style.backgroundColor = randomColor;
-  colorChange.textContent = randomColor;
+  changeElem.style.backgroundColor = random;
+  colorChange.textContent = random;
 });
 
 
 
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-const body = document.body;
-const colorSpan = document.querySelector(".color");
-const changeColorButton = document.querySelector(".change-color");
-
-changeColorButton.addEventListener("click", () => {
-  const newColor = getRandomHexColor();
-  body.style.backgroundColor = newColor;
-  colorSpan.textContent = newColor;
-});
